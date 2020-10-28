c34a4d40368bf7d18ff5252f8a0bd8e06a3e03f8
Andre Andrade <andrereisandrade@gmail.com>
2020-10-27 21:32:27 -0300

    Initial code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import Card from './components/card';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> React Redux</h1>
+      <div className="linha">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,10 @@
+import './card.css';
+
+import React from 'react';
+
+
+export default props => (
+  <div>
+    <h1>test</h1>
+  </div>
+)
